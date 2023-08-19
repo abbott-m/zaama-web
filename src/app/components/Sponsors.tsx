528ee52ba04dfc4322d65b82e9aeecc0e33df518
Babatunde-Ben <babatundeben110@gmail.com>
2023-08-19 13:02:00 +0100

    style: modified sponsors section

--- src/app/components/Sponsors.tsx
+++ src/app/components/Sponsors.tsx
@@ -5,48 +5,48 @@ const Sponsors = () => {
   return (
     <section
       id="sponsors"
-      className="flex flex-wrap items-center justify-center my-5 gap-3  px-4 py-10  sm:px-28 sm:py-24 md:gap-8"
+      className="flex flex-wrap items-center justify-center my-5 gap-5  px-4 py-10  sm:px-28 sm:py-24 md:gap-8"
     >
       <Image
         src="/Blache_logo.png"
         alt="Blach-logo"
-        width={100}
-        height={100}
+        width={110}
+        height={110}
         className="md:w-36"
       />
       <Image
         src="/Blache_logo.png"
         alt="Blach-logo"
-        width={100}
-        height={100}
+        width={110}
+        height={110}
         className="md:w-36"
       />
       <Image
         src="/Blache_logo.png"
         alt="Blach-logo"
-        width={100}
-        height={100}
+        width={110}
+        height={110}
         className="md:w-36"
       />
       <Image
         src="/Blache_logo.png"
         alt="Blach-logo"
-        width={100}
-        height={100}
+        width={110}
+        height={110}
         className="md:w-36"
       />
       <Image
         src="/Blache_logo.png"
         alt="Blach-logo"
-        width={100}
-        height={100}
+        width={110}
+        height={110}
         className="md:w-36"
       />
       <Image
         src="/Blache_logo.png"
         alt="Blach-logo"
-        width={100}
-        height={100}
+        width={110}
+        height={110}
         className="md:w-36"
       />
     </section>
