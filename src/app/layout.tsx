634eea5e3dad49fc0ba0c322ae086c8b15a73c6c
Babatunde-Ben <babatundeben110@gmail.com>
2023-09-05 20:16:35 +0100

    refactor: loading screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
-"use client";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Syne_Mono } from "next/font/google";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import CarouselLabel from "./components/CarouselLabel";
-import Image from "next/image";
-import { useState, useEffect } from "react";
 
 const syneMono = Syne_Mono({ subsets: ["latin"], weight: "400" });
 
@@ -29,36 +23,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 6400);
-  }, []);
-
   return (
     <html lang="en">
       <body
         className={`${syneMono.className} bg-[#151515] text-gray-200 pt-3 max-w-[2200px] lg:mx-auto`}
       >
-        {loading ? (
-          <div className="h-screen w-screen fixed flex justify-center items-center">
-            <Image
-              src="/GIF/zaama-logo-anim.gif"
-              alt="zaama-logo-animation"
-              width={300}
-              height={100}
-            />
-          </div>
-        ) : (
-          <>
-            <CarouselLabel word="Zaama: The Disco" />
-            <Navbar />
-            {children}
-            <Footer />
-          </>
-        )}
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import About from "./components/About";
 import Sponsors from "./components/Sponsors";
 import Hero from "./components/Hero";
@@ -7,18 +8,43 @@ import Store from "./components/Store";
 import Ticket from "./components/Ticket";
 import CarouselLabel from "./components/CarouselLabel";
 import Image from "next/image";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 6500);
+  }, []);
   return (
     <main className="">
-      <Hero />
-      <CarouselLabel word="tickets" />
-      <Ticket />
-      <CarouselLabel word="about" />
-      <About />
-      <Store />
-      <Sponsors />
-      <FAQ />
+      {loading ? (
+        <div className="h-screen w-screen fixed flex justify-center items-center">
+          <Image
+            src="/GIF/zaama-logo-anim.gif"
+            alt="zaama-logo-animation"
+            width={300}
+            height={100}
+          />
+        </div>
+      ) : (
+        <>
+          <CarouselLabel word="Zaama: The Disco" />
+          <Navbar />
+          <Hero />
+          <CarouselLabel word="tickets" />
+          <Ticket />
+          <CarouselLabel word="about" />
+          <About />
+          <Store />
+          <Sponsors />
+          <FAQ />
+          <Footer />
+        </>
+      )}
     </main>
   );
 };
